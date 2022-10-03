400bdec9b3678389d2f500cfeea77a005f8d8cf1
Prometheus <poligraphich@gmail.com>
2022-10-03 19:15:38 +0300

    add exampl001

---
+++ example001_hw9/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 64: Задайте значение N. Напишите программу,
+// которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+Console.WriteLine("Enter number N: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(PrintNumber(1, num));
+
+string PrintNumber(int start, int end)
+{
+    if (start == end)
+    {
+        return start.ToString();
+    }
+    return(start + "," + PrintNumber(start + 1, end));
+}
